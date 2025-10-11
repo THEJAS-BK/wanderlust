882d588cd5d94bc08acf66bdce68f0cb6edd6c19
THEJAS-BK <thejasbk998@gmail.com>
2025-10-11 17:57:40 +0530

    test

--- middleware.js
+++ middleware.js
@@ -20,10 +20,6 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
   let { id } = req.params;
   let test = await Listing.findById(id);
-  console.log("Isowner:", test);
-  console.log("test.owner:", test.owner);
-  console.log("currentUser:", res.locals.currentUser);
-
   if (
     !test ||
     !test.owner ||
